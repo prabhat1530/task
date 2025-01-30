f145a7514e95afccc80f7660c53c0dde4f08c93b
prabhat1530 <152500533+prabhat1530@users.noreply.github.com>
2025-01-30 12:13:05 +0530

    up

--- Frontend/todo/src/components/TodoList.jsx
+++ Frontend/todo/src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
+import '../App.css'
 
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
